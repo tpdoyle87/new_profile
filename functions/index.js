a670ced860a23ea5e0239a288666155ea64abf57
Thomas DoyleEngler <tpdoyle87@gmail.com>
2019-01-07 21:16:38 -0500

    functions

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,17 @@
 
 const functions = require('firebase-functions');
 const nodemailer = require('nodemailer');
+
+var config = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
+
+};
+firebase.initializeApp(config);
 // Configure the email transport using the default SMTP transport and a GMail account.
 // For other types of transports such as Sendgrid see https://nodemailer.com/transports/
 // TODO: Configure the `gmail.email` and `gmail.password` Google Cloud environment variables.
