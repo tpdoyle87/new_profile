37e6aee684948c2156baaffaf596e0398989c678
Thomas DoyleEngler <tpdoyle87@gmail.com>
2019-01-10 13:31:42 -0500

    spelling corrections

--- src/pages/aboutpage.js
+++ src/pages/aboutpage.js
@@ -13,13 +13,13 @@ export default class AboutPage extends Component {
           <SectionIntro className="section-intro" text="Professional" />
           <div className="content-container">
             <Section
-              text="When it comes to my professional career I have been building things since I was a kid. It started out with physical items, from a wall for a room partition, a house and eventually oil refineries. In the past couple years I have transitioned from building, buildings to building software"
+              text="When it comes to my professional career I have been building things since I was a kid. It started out with physical items, from a wall for a room partition, a house and eventually oil refineries. In the past couple years I have transitioned from building buildings to building software."
             />
             <Section
-              text="The transition has not been without obstacles. I have spent almost everyday off I have had over the past few years learning something new, building some new small feature, and reading blogs realted to software development. I focus mainly on rails applications but, recently I have expanded to React apps as well."
+              text="The transition has not been without obstacles. I have spent almost everyday off I have had over the past few years learning something new, building some new small feature, and reading blogs related to software development. I focus mainly on rails applications but recently I have expanded to React apps as well."
             />
             <Section
-            text="I take great pride in bringing craftsmanship to software engineering. If you would like to know more about me or you have a project you need help on, let's talk!"
+              text="I take great pride in bringing craftsmanship to software engineering. If you would like to know more about me or you have a project you need help on let's talk!"
             button={true}
             />
           </div>
@@ -33,7 +33,7 @@ export default class AboutPage extends Component {
             text="When i'm not working I like to spend my time doing a variety of different activities. I enjoy skiing and snowboarding, hiking, and searching out new experiences."
           />
           <Section
-            text="One thing i find i'm always doing is learning, I can't stop trying to improve myself. I seem to have some compulsion to understand how everything works and to learn something new everyday."
+            text="One thing I find i'm always doing is learning, I can't stop trying to improve myself. I seem to have some compulsion to understand how everything works and to learn something new everyday."
           />
         </div>
         <SectionIntro className="section-intro" text="Experience" />
